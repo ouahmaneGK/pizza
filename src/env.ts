010b2a3805d1ec27b7b7e48f3be0bc9b6248d9fb
ouahmaneGK <52911887+ouahmaneGK@users.noreply.github.com>
2021-04-14 13:47:57 +0200

    add tests docker file update docker compse

--- src/env.ts
+++
@@ -1,7 +0,0 @@
-import path from "path";
-
-if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test" ) {
-  require("dotenv-expand")(
-    require("dotenv").config({ path: path.join(__dirname, "..", ".env") })
-  );
-}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import "./env";
+
 import { router } from "./api";
 
 const app = express();
