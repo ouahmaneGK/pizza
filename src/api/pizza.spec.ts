60c41726370e8d2b4e6fa2d58c36094a511cd30c
ouahmaneGK <52911887+ouahmaneGK@users.noreply.github.com>
2021-04-15 12:46:28 +0200

    testfile corrected

--- src/api/pizza.spec.ts
+++ src/api/pizza.spec.ts
@@ -3,7 +3,7 @@ import { router } from "./pizzas";
 import supertest from "supertest";
 import express from "express";
 import { database } from "../database";
-describe('api/', () => {
+describe('api/pizzas', () => {
    beforeAll(async () => {
       await database.migrate.latest();
    })
@@ -11,10 +11,7 @@ describe('api/', () => {
       await database.destroy();
    });
 
-   describe('Post/ insert pizza', () => {
-      
-     
-    
+   //describe('Post/ insert pizza', () => {
       it('should create a new pizza', async () => {
           
          const request = supertest(express().use(express.json()).use(router))
@@ -26,24 +23,20 @@ describe('api/', () => {
          expect(result.status).toEqual(200)
          expect(result.body).toMatchObject({name:'pizzaTest', price:'15'});
          
-      })
+      }) 
+   //})
    
-   
-   
-   
-   })
-   
-   describe('Get/ get all of pizzas', () => {
+//describe('Get/ get all of pizzas', () => {
    
       it('should return a list of pizza', async () => {
           
          const request = supertest(express().use(router))
          const results = await request.get("/pizzas");
-         expect(results.body.length).toBeGreaterThan(1);
+         expect(results.body.length).toBeGreaterThan(0);
          
       })
    
-   })
+   //})
    
    
  }
