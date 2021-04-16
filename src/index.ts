7318a408b998dbe617ef44e9e23f9afc286c8585
ouahmaneGK <52911887+ouahmaneGK@users.noreply.github.com>
2021-04-16 17:13:25 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
 import { router } from "./api";
-
+import cors from "cors";
+import bodyParser from "body-parser";
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(router);
+
 
 app.listen(process.env.PORT);
